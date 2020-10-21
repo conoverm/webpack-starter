1acb87c30343ecba37eaa004b7572d99a3ad5607
MC <conoverm@gmail.com>
2020-10-20 22:30:11 -0600

    import { join } from lodash-es:wq

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { join } from 'lodash';
+import { join } from 'lodash-es';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = join(['Hello', 'webpack'], ' ');
 
   return element;
 }
